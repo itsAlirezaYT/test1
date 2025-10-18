bfbc838f44e4f27083f07002e21bed5255d6cd9f
itsAlirezaYT <ytvision.compani@gmail.com>
2025-10-18 06:31:24 -0700

    Update uninstall.bat

--- uninstall.bat
+++ uninstall.bat
@@ -1,9 +1,8 @@
 @echo off
 setlocal
-set "TARGET=C:\ProgramData\MyApp"
+set "TARGET=%LOCALAPPDATA%\MyApp"
 set "EXENAME=system38.exe"
 
-del /F /Q "%TARGET%\%EXENAME%"
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "MyApp" /f
-schtasks /delete /tn "MyAppStartup" /f
-rmdir /S /Q "%TARGET%"
+del /F /Q "%TARGET%\%EXENAME%" >nul 2>&1
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "MyApp" /f >nul 2>&1
+rmdir /S /Q "%TARGET%" >nul 2>&1
